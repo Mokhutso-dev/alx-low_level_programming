a0a245325cd82025e865516e370eb0561acc045b
Mokhutso-dev <manaswemahlako@gmail.com>
2022-09-09 19:14:29 -0700

    No spaces , Betty style way

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)  
-{  
-  char letter;    
-  for (letter = 'a'; letter <= 'z'; letter++)    
-    {      
-      if (letter != 'e' && letter != 'q')	
-	putchar(letter);     
-    }  
-  putchar('\n');    
-  return (0);  
+{
+char letter;    
+for (letter = 'a'; letter <= 'z'; letter++)    
+{      
+if (letter != 'e' && letter != 'q')	
+putchar(letter);     
+}  
+putchar('\n');    
+return (0);  
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-  int digit;
-  for (digit = 0; digit < 10; digit++)
-    
-    printf("%i", digit);
-  
-  putchar('\n');
-  return (0);
+int digit;
+for (digit = 0; digit < 10; digit++)    
+printf("%i", digit);  
+putchar('\n');
+return (0);
 }
