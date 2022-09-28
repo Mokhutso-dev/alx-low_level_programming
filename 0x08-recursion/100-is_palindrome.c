a6481a2a0d5b2cc2c30b52da00fba7865b652662
Mokhutso-dev <manaswemahlako@gmail.com>
2022-09-28 03:29:26 -0700

    Recursion and Factorials

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,10 +1,5 @@
 #include "main.h"
-/**
- * wildcmp - compares two strings
- * @s1: 1st string to be compared
- * @s2: second string containing '*' or not
- * Return: 1 if the strings can be considered as identical, and 0
- **/
+#include <stdio.h>
 int is_palindrome(char *s)
 {
   if (*s1 == '\0' && *s2 == '\0')
