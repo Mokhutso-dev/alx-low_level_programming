c158338582af5987b196520894320229b057b646
Mokhutso-dev <manaswemahlako@gmail.com>
2022-09-28 03:36:00 -0700

    Recursion and Factorial

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,43 +1,21 @@
 #include "main.h"
-/**
- * _strlen_recursion - finds the length of a string
- * @s: pointer to string
- * Return: length of string
- **/
-int _strlen_recursion(char *s)
+#include <stdio.h>
+int is_prime_number(int n)
 {
-  if (*s == '\0')
-    return (0);
-  return (_strlen_recursion(s + 1) + 1);
-}
-/**
- * is_palind_recursive - checks if two chars of a string are equal
- * @s: string to be checked
- * @i: first index
- * @j: last index
- * Return: 1 if equal, else 0
- **/
-int is_palind_recursive(char *s, int i, int j)
-{
-  if (i == j)
+  if (n == 1)
     return (1);
-  if (i == j - 1)
-    return (s[i] == s[j]);
-  if (s[i] != s[j])
-    return (0);
-  return (is_palind_recursive(s, i + 1, j - 1));
+  return (sqtRecursive(n / 2, n));
 }
 /**
- * is_palindrome - checks if a string is a palindrome
- * @s: pointer to string
- * Return: 1 if string is palindrome  else 0
+ * is_prime_number - checks if a given number is prime
+ * @n: given number
+ * Return: 1 if number is prime else 0
  **/
-int is_palindrome(char *s)
+int is_prime_number(int n)
 {
-  int len;
-
-  len = _strlen_recursion(s);
-  if (len == 0 || *s != s[len - 1])
+  if (n <= 1 || _sqrt_recursion(n) >= 1)
     return (0);
-  return (is_palind_recursive(s, 0, len - 1));
+  if (_sqrt_recursion(n) == -1)
+    return (1);
+  return (_sqrt_recursion(n));
 }
