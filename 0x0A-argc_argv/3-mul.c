fc9082c5e07ac6892944ac326bc4a581ad1db0de
Mokhutso-dev <manaswemahlako@gmail.com>
2022-09-30 04:11:05 -0700

    C - argc, argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+int a, b, result;
+if (argc <= 2)
+{
+printf("Error\n");
+return (1);
+}
+/**
+atoi convert a string to an int */
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+result = a * b;
+printf("%d\n", result);
+return (0);
+}
